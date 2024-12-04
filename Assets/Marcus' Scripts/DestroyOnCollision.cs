096bb7429b0e835a73c9e7c984b522ab535acf77
flyingcratesdev <joeflydev@gmail.com>
2024-12-04 03:41:28 -0800

    IceRink

--- Assets/Marcus' Scripts/DestroyOnCollision.cs
+++ Assets/Marcus' Scripts/DestroyOnCollision.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class DestroyOnCollision : MonoBehaviour
 {
+    public GameObject door;
+
     // Trigger detection for player collision
     private void OnTriggerEnter(Collider other)
     {
@@ -9,28 +11,14 @@ public class DestroyOnCollision : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             // Find and destroy the object tagged as "Door"
-            GameObject door = GameObject.FindWithTag("Door");
             if (door != null)
             {
                 Destroy(door); // Destroy the door object
                 Debug.Log("Door destroyed.");
             }
-            else
-            {
-                Debug.LogError("No object found with tag 'Door'.");
-            }
 
-            // Find and destroy the object tagged as "Key"
-            GameObject key = GameObject.FindWithTag("Key");
-            if (key != null)
-            {
-                Destroy(key); // Destroy the key object
-                Debug.Log("Key destroyed.");
-            }
-            else
-            {
-                Debug.LogError("No object found with tag 'Key'.");
-            }
+
+            Destroy(this.gameObject);
         }
     }
 }
